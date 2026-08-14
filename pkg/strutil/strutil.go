b5f81eb872dfcbe4c13131a73367122eb2d92065
xytan0056 <48696694+xytan0056@users.noreply.github.com>
2026-08-13 22:53:58 -0700

    feat: add timeutil + audit proto, drop mathutil, extend strutil

--- pkg/strutil/strutil.go
+++ pkg/strutil/strutil.go
@@ -38,3 +38,11 @@ func TitleWords(s string) string {
 	}
 	return strings.Join(words, " ")
 }
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
